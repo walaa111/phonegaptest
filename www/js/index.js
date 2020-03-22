c7b87a7dcf1fe359f24fe5ced4277c73ba0df79c
walaa111 <walaagamil18@gmail.com>
2020-03-22 16:02:30 +0200

    edit

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,24 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+        // Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  alert("test");
+  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("1efd3206-9517-4006-b656-8fd0e9465981")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
     },
 
     // Update DOM on a Received Event
