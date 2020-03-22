8626ad3bdca1d7aa13342df6bdd66315753d8d20
walaa111 <walaagamil18@gmail.com>
2020-03-22 16:06:39 +0200

    edit

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "onesignal-cordova-plugin.OneSignal",
+      "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+      "pluginId": "onesignal-cordova-plugin",
+      "clobbers": [
+        "OneSignal"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "onesignal-cordova-plugin": "2.8.4",
+    "cordova-plugin-whitelist": "1.3.4"
+  };
+});
